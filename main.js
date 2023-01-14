92b3d9f475a0dffe3ae3c609cc8d6c782f6c9f38
rehab <rehabmohamedaziz99@gmail.com>
2023-01-15 00:47:59 +0200

    javascript2 html

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const colors = ["navy" , "white" , "rgba(133,122,200)", "#f15025"];
+const colors = ["navy", "white", "rgba(133,122,200)", "#f15025"];
 
 let btn = document.getElementById("btn");
 let color = document.querySelector(".color");
 
-btn.addEventListener("click",function(){
-    const randomNum = getRandomNum();
-    document.body.style.backgroundColor=colors[randomNum];
-    color.textContent=colors[randomNum];
-})
+btn.addEventListener("click", function () {
+  const randomNum = getRandomNum();
+  document.body.style.backgroundColor = colors[randomNum];
+  color.textContent = colors[randomNum];
+});
 
-function getRandomNum(){
-    return Math.floor(Math.random()* colors.length);
+function getRandomNum() {
+  return Math.floor(Math.random() * colors.length);
 }
